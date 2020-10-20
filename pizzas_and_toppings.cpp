8f07ecb1e5c5216fb67a11f950da50d074656500
faruk <faruk@anyconnect.com>
2020-10-20 12:00:01 +0600

    Pizza and toppings fixed

--- pizzas_and_toppings.cpp
+++ pizzas_and_toppings.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <algorithm>
 #include <unordered_map>
+#include <climits>
 
 
 using namespace std;
@@ -13,31 +14,46 @@ using namespace std;
 class PizzaAndToppings {
 public:
 
-    bool needToDecrease(const int a, const int b) {
-        mn = min(mn, remaining);
-        mn = min(mn, abs(a - remaining));
-        mn = min(mn, abs(b - remaining));
-        mn = min(mn, abs(a + b - remaining));
+    bool needToDecrease(const int a, const int b, const int cur) {
+        if (remaining < mn) {
+            mn = min(mn, remaining);
+            ans = cur;
+        }
+        if (abs(a - remaining) < mn) {
+            mn = min(mn, abs(a - remaining));
+            ans = cur + a;
+        }
+        if (abs(b - remaining) < mn) {
+            mn = min(mn, abs(b - remaining));
+            ans = cur + b;
+        }
+        if (abs(a + b - remaining) < mn) {
+            mn = min(mn, abs(a + b - remaining));
+            ans = cur + a + b;
+        }
 
         if ((a+b) >= remaining) return true;
         return false;
     }
 
-    int groupStrings(const vector<int> pizzas, vector<int> toppings, const int x) {
+    int getMinCost(const vector<int> pizzas, vector<int> toppings, const int x) {
+        remaining = 0;
+        mn = INT_MAX;
+        ans = -1;
         sort(toppings.begin(), toppings.end());
         for(auto cur : pizzas) {
             remaining = x - cur;
             if (remaining < 0) continue;
 
             for(int i = 0, j = toppings.size() - 1; i <= j; i++, j--) {
-                if (needToDecrease(toppings[i], toppings[j])) {
+                if (needToDecrease(toppings[i], toppings[j], cur)) {
                     j--;
                 } else {
                     i++;
                 }
             }
         }
-        return mn;
+        return ans;
     }
 
 
@@ -45,21 +61,17 @@ public:
 private:
     int remaining = 0;
     int mn = INT_MAX;
+    int ans = -1;
 };
 int main()
 {
-    vector<string> inp({"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"});
+    vector<int> pizzas({800, 850, 900});
+    vector<int> toppings({100, 150});
 
-    vector<vector<string>> ans = groupStrings(inp);
+    PizzaAndToppings pt;
 
-    cout << (int)ans.size() << endl;
-
-    for (auto element : ans) {
-        for(auto v : element) {
-            cout << v << " ";
-        }
-        cout << endl;
-    }
+    cout << pt.getMinCost(pizzas, toppings, 1000) << endl;
+    cout << pt.getMinCost({850, 900}, {200, 250}, 1000) << endl;
 
     return 0;
 }
