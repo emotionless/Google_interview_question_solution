55890a2c2903805d4c10ea7fdbb0d10fe62eafbb
Faruk Hossain Milon <miloniitju@gmail.com>
2020-10-29 18:32:07 +0600

    Best fruit added...

--- best_fruit.cpp
+++ best_fruit.cpp
@@ -27,44 +27,64 @@ void pop_front(vector<T> &vec) {
     return;
 }
 
+vector<vector<int>> mat;
+
 int main()
 {
     int n, m;
     cin >> m >> n;
     vector<vector<int>> arr(n, vector<int>(m, -1));
     valid.resize(m + 1, true);
+    mat.resize(m+1);
+    int counter[m+1];
+    memset(counter, 0, sizeof counter);
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> arr[i][j];
+            if (j == 0) {
+                counter[ arr[i][j] ]++;
+                mat[ arr[i][j] ].push_back(i);
+            }
         }
     }
-    int counter[m+1];
+    for(int i = 1; i <= m; i++) {
+        container.insert(make_pair(counter[i], i));
+    }
     int num = 0;
+
     for(int k = 1; k < m ; k++) {
-        memset(counter, 0, sizeof counter);
-        for(int i = 0; i < n; i++) {
+        for(auto i : mat[num]) {
             while(!isValid(arr[i][0])) {
                 pop_front(arr[i]);
             }
-//            cout << i << " " << arr[i][0] << endl;
-            counter[ arr[i][0] ]++;
+            int fruit = arr[i][0];
+            auto it = container.find(make_pair(counter[fruit], fruit));
+            container.erase(it);
+            counter[ fruit ]++;
+            container.insert(make_pair(counter[fruit], fruit));
+            mat[ fruit ].push_back(i);
         }
         int cnt = INT_MAX;
-        for(int i = 1; i <= m; i++) {
-            if (!isValid(i)) {
-                continue;
-            }
-            if (counter[i] < cnt) {
-                cnt = counter[i];
-                num = i;
-            }
-        }
+        num = -1;
+        auto it = container.begin();
+        num = it->second;
+//        for(int i = 1; i <= m; i++) {
+//            if (!isValid(i)) {
+//                continue;
+//            }
+//            if (counter[i] < cnt) {
+//                cnt = counter[i];
+//                num = i;
+//            }
+//            cout << i << " " << counter[i] << endl;
+//        }
         if (num == -1) assert(0);
 
         valid[num] = false;
-//        cout << num << endl;
+//        cout << num << " " << it->first << endl;
+        container.erase(it);
     }
-
+    cout << "Ans: " << endl;
     for(int i = 1; i <= m; i++) {
         if (isValid(i)) {
             cout << i << endl;
@@ -87,6 +107,7 @@ input:
 3 4 2 1
 4 3 2 1
 
+2 1
 2 1
 
 Output:
