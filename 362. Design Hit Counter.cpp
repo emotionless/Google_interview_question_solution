07ed122c5a0cf6498f23156efd511df20e98fa3a
Faruk Hossain Milon <miloniitju@gmail.com>
2020-12-24 02:29:27 +0600

    362 Design Hit Counter added

---
+++ 362. Design Hit Counter.cpp
@@ -0,0 +1,87 @@
+/*
+Problem link:  https://leetcode.com/problems/design-hit-counter/
+
+Design a hit counter which counts the number of hits received in the past 5 minutes.
+
+Each function accepts a timestamp parameter (in seconds granularity) and you may assume that calls are being made to the system in chronological order (ie, the timestamp is monotonically increasing). You may assume that the earliest timestamp starts at 1.
+
+It is possible that several hits arrive roughly at the same time.
+
+Example:
+
+HitCounter counter = new HitCounter();
+
+// hit at timestamp 1.
+counter.hit(1);
+
+// hit at timestamp 2.
+counter.hit(2);
+
+// hit at timestamp 3.
+counter.hit(3);
+
+// get hits at timestamp 4, should return 3.
+counter.getHits(4);
+
+// hit at timestamp 300.
+counter.hit(300);
+
+// get hits at timestamp 300, should return 4.
+counter.getHits(300);
+
+// get hits at timestamp 301, should return 3.
+counter.getHits(301);
+Follow up:
+What if the number of hits per second could be very large? Does your design scale?
+
+
+*/
+
+// solved by Milon
+
+class HitCounter {
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        counter.clear();
+        sum = 0;
+    }
+
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(const int timestamp) {
+        counter[timestamp]++;
+        container.insert(timestamp);
+        sum++;
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(const int timestamp) {
+        int last = timestamp - 300 + 1;
+        while(!container.empty()) {
+            auto it = container.begin();
+            if (*it < last) {
+                int num = *it;
+                sum -= counter[num];
+                counter.erase(num);
+                container.erase(it);
+            }
+            else {
+                break;
+            }
+        }
+        return sum;
+    }
+private:
+    unordered_map<int, int> counter;
+    set<int> container;
+    int sum = 0;
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
